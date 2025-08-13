7c7ff8351aeb9477f6b5514d1c818c25fe5a48d4
Raimundojr <raymundojunior.dev@gmail.com>
2025-08-13 10:45:37 -0300

    atualização do código pronto

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function atualizarRelogio(){
     const agora = new Date();
-
-
-
-
 let horas = agora.getHours()
 let minutos = agora.getMinutes()
-let segundos = agora.getSeconds()}
+let segundos = agora.getSeconds()
+
+horas = horas < 10 ? "0" + horas : horas;
+minutos = minutos < 10 ? "0" + minutos : minutos;
+segundos = segundos< 10 ? "0" + segundos : segundos;
+
+document.getElementById("Relogio").textContent = 
+    `${horas}:${minutos}:${segundos}`;
+}
+
+setInterval(atualizarRelogio, 1000);
+atualizarRelogio();
